25ff40b089d441377c6e3f8b2a491981ee3ff444
Šárka F. <sarka.flidrova@email.cz>
2024-12-09 13:18:28 +0100

    First crawl - collect product titles.

--- main.js
+++ main.js
@@ -24,12 +24,33 @@ const $ = cheerio.load(html);
 // Find all the links on the website
 const productLinks = $("a.product-item__title");
 
+// Empty array for product URLs
+const productUrls = [];
+
 for (const link of productLinks) {
   const relativeUrl = $(link).attr("href");
-
   // Resolve relative URLs
   const absoluteUrl = new URL(relativeUrl, WEBSITE_URL);
-  const url = absoluteUrl.href;
+  productUrls.push(absoluteUrl);
+}
 
-  console.log(url);
+// Loop over the stored URLs to process each product page individually
+for (const url of productUrls) {
+  // Add try / catch error for error handling
+  try {
+    // Download html
+    const productResponse = await gotScraping(url);
+    const productHtml = productResponse.body;
+
+    // Load into Cheerio to parse the HTML
+    const $productPage = cheerio.load(productHtml);
+
+    // Extract the product's title from the h1 tag
+    const productPageTitle = $productPage("h1").text().trim();
+
+    // Print the title to the terminal to confirm we downloaded correct pages
+    console.log(productPageTitle);
+  } catch (error) {
+    console.error(error.message, url);
+  }
 }
